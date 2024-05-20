3449f2189ee707e179fabf03298aad90c5c48750
Lighttx26 <ncnhatquang26@gmail.com>
2024-05-20 10:06:17 +0700

    Format float price

--- cart/context_processor.py
+++ cart/context_processor.py
@@ -8,5 +8,6 @@ def cart_custom_processor(request):
     cart = cart_services.get_cart(request.user)
     books_in_cart = cart_services.books_in_cart(cart)
     return {
+        'cart': cart,
         'books_in_cart': books_in_cart,
     }
--- cart/views.py
+++ cart/views.py
@@ -17,9 +17,8 @@ def add_to_cart(request):
 
 @login_required
 def view_cart(request):
-    cart = cart_services.get_cart(request.user)
-    books_in_cart = BookInCart.objects.filter(cart=cart)
-    return render(request, 'cart.html', {'books': books_in_cart})
+    return render(request, 'cart/cart_items.html')
+
 
 @login_required
 def remove_from_cart(request):
@@ -31,6 +30,7 @@ def remove_from_cart(request):
     cart.remove(book_id)
     return redirect('view_cart')
 
+
 @login_required
 def update_quantity(request):
     if request.method != 'POST':
@@ -41,6 +41,7 @@ def update_quantity(request):
     cart = cart_services.get_cart(request.user)
     cart.update_quantity(book_id, quantity)
     return JsonResponse({'status': 'success'})
+
 
 @login_required
 def get_cart_size(request):
