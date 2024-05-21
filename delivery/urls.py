cf9a54ba94d37f07beef8938c5d08e8c7c8e7dda
Lighttx26 <ncnhatquang26@gmail.com>
2024-05-21 11:41:26 +0700

    fix url name

--- delivery/urls.py
+++ delivery/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', view_arrived_shipment, name='view_arrived_shipments'),
-    path('success', view_success_shipment, name='history_shipments'),
-    path('delivering', view_delivering_shipment, name='history_shipments'),
+    path('arrived', view_arrived_shipment, name='view_arrived_shipments'),
+    path('success', view_success_shipment, name='view_success_shipment'),
+    path('delivering', view_delivering_shipment, name='view_delivering_shipment'),
     path('take/<int:shipment_id>', take_shipment, name='take_shipment'),
     path('return/<int:shipment_id>', return_shipment, name='return_shipment'),
     path('complete/<int:shipment_id>', complete_shipment, name='complete_shipment'),
